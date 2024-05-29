ae491c4d48cb8c57e0473bbe99ad304425c3b1bf
lee duck youn <chunchon3121@naver.com>
2024-05-29 16:05:33 +0900

    초기화조건문

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,18 @@ function App() {
   const 리턴값 = useState(0);
   const count = 리턴값[0];
   const setCount = 리턴값[1];
+
+  let 초기화버튼;
+  if (count > 0) {
+    초기화버튼 = <button onClick={() => setCount(0)}>초기화</button>;
+  }
+
   return (
     <div>
       <h1>{count}</h1>
-      <button onClick={() => setCount((현재count) => 현재count + 1)}>
-        증가
-      </button>
-      <button onClick={() => setCount((현재count) => 현재count - 1)}>
-        감소
-      </button>
-      <button onClick={() => setCount(0)}>초기화</button>
+      <button onClick={() => setCount((count) => count + 1)}>증가</button>
+      <button onClick={() => setCount((count) => count - 1)}>감소</button>
+      {초기화버튼}
     </div>
   );
 }
