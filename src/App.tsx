2c49cafe08062454268e055f7aa02e0502a9aa9d
lee duck youn <chunchon3121@naver.com>
2024-05-29 12:50:57 +0900

    on클릭

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,11 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
+  let count = 0;
   return (
     <div>
       <h1>0</h1>
-      <button>증가</button>
+      <button onClick={() => console.log("클릭됨")}>증가</button>
     </div>
   );
 }
