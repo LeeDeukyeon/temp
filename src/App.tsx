1a1872409206e6c3af51d17b24bfca94877cbe1b
lee duck youn <chunchon3121@naver.com>
2024-05-29 13:03:10 +0900

    useSate

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,15 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  let count = 0;
+  const 리턴값 = useState(0);
+  const count = 리턴값[0];
+  const setCount = 리턴값[1];
   return (
     <div>
-      <h1>0</h1>
-      <button onClick={() => console.log("클릭됨")}>증가</button>
+      <h1>{count}</h1>
+      <button onClick={() => setCount((현재count) => 현재count + 1)}>
+        증가
+      </button>
     </div>
   );
 }
